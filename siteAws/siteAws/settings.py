9fe1e8438fb8249cb8e25ad13f843908efe96f40
FranciscoEljuri <franciscoeljuri99@gmail.com>
2024-08-23 17:20:47 -0400

    modificacion de la url

--- siteAws/siteAws/settings.py
+++ siteAws/siteAws/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0xjymqi!3(q6s%v)r&7#lgts%k(c0oosyat*s8($$6)sx&1s=r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
